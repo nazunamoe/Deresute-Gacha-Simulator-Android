8115ac651bcecde82df1e3e21b0da6157d52070e
nazunamoe <jpg3927@gmail.com>
2019-05-27 13:49:18 +0900

    디자인 초안

--- app/src/main/java/com/nazunamoe/deresutegachasimulatorm/Activity/MainActivity.java
+++ app/src/main/java/com/nazunamoe/deresutegachasimulatorm/Activity/MainActivity.java
@@ -1,21 +1,64 @@
 package com.nazunamoe.deresutegachasimulatorm.Activity;
 
+import android.icu.text.IDNA;
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ViewUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
+import com.nazunamoe.deresutegachasimulatorm.Fragments.GachaFragment;
+import com.nazunamoe.deresutegachasimulatorm.Fragments.InfoFragment;
+import com.nazunamoe.deresutegachasimulatorm.Fragments.MoneyFragment;
 import com.nazunamoe.deresutegachasimulatorm.R;
 
-public class MainActivity extends AppCompatActivity {
-    private TabLayout mTabLayout;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements GachaFragment.OnFragmentInteractionListener, InfoFragment.OnFragmentInteractionListener, MoneyFragment.OnFragmentInteractionListener {
+
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTabLayout = (TabLayout) findViewById(R.id.layout_tab);
 
-        mTabLayout.addTab(mTabLayout.newTab().setText(R.string.MoneyMenu));
-        mTabLayout.addTab(mTabLayout.newTab().setText(R.string.GachaMenu));
-        mTabLayout.addTab(mTabLayout.newTab().setText(R.string.InfoMenu));
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.layout_tab);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.MoneyMenu));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.GachaMenu));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.InfoMenu));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.Viewpage);
+        final PageAdapter adapter = new PageAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
