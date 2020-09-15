e15f7fb3ddaae5f15a79196460cc1cd62b5dca50
nazunamoe <nakashimayuki@kakao.com>
2020-09-15 22:24:32 +0900

    가챠 알고리즘 수정

--- app/src/main/java/com/nazunamoe/deresutegachasimulatorm/Gacha/Gacha.java
+++ app/src/main/java/com/nazunamoe/deresutegachasimulatorm/Gacha/Gacha.java
@@ -8,15 +8,13 @@ public class Gacha {
     Random random = new Random();
 
     public int GachaExecute(float SSRp, float SRp, Boolean rensya){
-        int result;
         int temp;
         temp=random.nextInt(MAX);
-        if(temp<MAX*(SSRp*0.01)) result = 7;
-        else if(temp<MAX*(SRp*0.01)) result = 5;
+        if(temp<MAX*(SSRp*0.01)) return 7;
+        else if(temp<MAX*(SRp*0.01)) return 5;
         else {
             if(rensya) return GachaExecute(SSRp, SRp, rensya);
-            else result = 3;
+            else return 3;
         }
-        return result;
     }
 }
